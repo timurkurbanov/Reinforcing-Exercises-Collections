174dd953f06a382f28f1e075a7103cf957b28d64
TIMUR <1tkurbanov@gmail.com>
2019-05-21 14:33:24 -0400

    done

---
+++ ex.py
@@ -0,0 +1,29 @@
+new_space = { 'data': { 'rooms': 
+    [ { 'id': 1, 'room_number': "201", 'capacity': 50}, 
+    { 'id': 2, 'room_number': "301", 'capacity': 200 }, 
+    { 'id': 3, 'room_number': "1B", 'capacity': 100}
+    ],
+    'events': [
+      { 'id': 1, 'room_id': 2, 'start_time': 18, 'end_time': 20, 'attendees': 75 },
+      { 'id': 2, 'room_id': 1, 'start_time': 10, 'end_time': 18, 'attendees': 25 },
+      { 'id': 3, 'room_id': 2, 'start_time': 10, 'end_time': 18, 'attendees': 20 },
+      { 'id': 4, 'room_id': 3, 'start_time': 18, 'end_time': 21, 'attendees': 56 },
+    ]
+  }
+}
+#Retrieve the capacity of room 201 and store it in a variable.
+room_201 = new_space["data"],["rooms"],0,["capacity"]
+print(room_201)
+
+#Find all the events taking place in room 201. 
+#Iterate through them and print "ok" if the number of planned 'attendees' will fit in the room.
+def check():
+    room = new_space['data']['rooms'][0]
+    for event in new_space["data"]["events"]:
+        if room['id'] == event['room_id']:
+            if event['attendees'] <= room['capacity']:
+                print(f"{event['id']}: will feet")
+        # else:
+        #     print(f"{event['id']}: will not feet") 
+check()
+
